5ffa5c41f1df029fc15fc00e3d5bf255bd6a6a0a
riadhulekar <riadhulekar@gmail.com>
2021-06-19 14:33:17 -0500

    updates to solution

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -11,7 +11,7 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
     private int defaultPasswordLength = 10;
     private String email;
     private String alternateEmail;
@@ -21,9 +21,7 @@ public class Email {
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
-
-    
+        
         //Call a method asking for the department and return the department 
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
@@ -33,16 +31,15 @@ public class Email {
         System.out.println("Your password is: " + this.password);
 
         //Combine elements to generate email
-        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + companySuffix;
-        System.out.println("Your email is: " + email);
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department +"." + companySuffix;
         
-
     }
     //Ask for the department
     private String setDepartment() {
         System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
         Scanner in = new Scanner (System.in);
         int depChoice = in.nextInt();
+        in.close();
         if (depChoice ==1){ return "sales";}
         else if (depChoice == 2){ return "dev";}
         else if (depChoice == 3){ return "acct";}
@@ -76,5 +73,13 @@ public class Email {
         this.password = password;
     }
 
-    
+    public int getMailboxCapacity() { return mailboxCapacity;}
+    public String getAlternateEmail() {return alternateEmail;}
+    public String getPassword() { return password;}
+
+    public String showInfo() {
+        return "DISPLAY NAME: "+ firstName + " " + lastName + 
+                "\nCOMPANY EMAIL: " + email + 
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    }
 }
--- emailapp/emailApp.java
+++ emailapp/emailApp.java
@@ -1,9 +1,13 @@
+//Ria Dhulekar 
+//Practice Java project for OOP
+//from https://www.youtube.com/watch?v=U3Ibvu0htNs&t=133s
+
 package emailapp;
 
 public class emailApp {
     public static void main(String [] args){
         Email eml = new Email ("John", "Smith");
-        
+        System.out.println(eml.showInfo());
     }
-}
 
+}
